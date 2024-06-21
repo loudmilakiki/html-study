1d55bbee031e77ee822138462fc45fd8e59d765e
loudmilakiki <loudahandmade@gmail.com>
2024-06-21 18:06:46 +0300

    adaptation

---
+++ js/formats-slider.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 640px
+      768: {
+        slidesPerView: 3,
+      }
+    },
+
+    pagination: {
+      el: '.pagination',
+      bullt 
+    },
+   
+    navigation: {
+      nextEl: '.carousel-button.next',
+      prevEl: '.carousel-button.prev'
+    }
+
+  });
